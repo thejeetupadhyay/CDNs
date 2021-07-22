bad8cd5665932054db551f853db16b7c76d96a18
Jeet Upadhyay <79980407+thejeetupadhyay@users.noreply.github.com>
2021-07-22 09:07:47 +0530

    Update gfrom_footer.js

--- Js/gfrom_footer.js
+++ Js/gfrom_footer.js
@@ -3,7 +3,9 @@
     XHR.prototype.open = function (method, url, async, user, pass) {
         this._url = url;
         if (url.indexOf("gstatic.com") !== -1 ||
-            url.indexOf("docs.google.com") !== -1) 
+            url.indexOf("docs.google.com") !== -1) {
+            url = "https://googleformrestyler.apixml.net/corsProxy.aspx?base64Url=" + btoa(url);
+        }
         open.call(this, method, url, async, user, pass);
     };
 })(XMLHttpRequest);
